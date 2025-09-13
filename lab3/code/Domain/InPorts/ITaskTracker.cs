a7150e5b65693e38ae1c18fb50892a4c862696b5
Kulikov Egor <kulikovegorandreevich@mail.ru>
2025-09-13 11:28:18 +0300

    added async in task tracker

--- lab3/code/Domain/InPorts/ITaskTracker.cs
+++ lab3/code/Domain/InPorts/ITaskTracker.cs
@@ -4,7 +4,16 @@ namespace Domain.InPorts;
 
 public interface ITaskTracker
 {
-    //По имени пользователя, телефону и паролю создает нового пользователя или возвращает null если пользователь уже есть
+    Task<User> CreateUserAsync(string username, PhoneNumber phone_number, string password);
+    Task<User> LogInAsync(string username, string password);
+    Task<Tuple<User, List<Habit>>> ImportNewSheduleAsync(string user_name, string path);
+    Task<Tuple<User, List<Habit>>> AddHabitAsync(Habit habit);
+    Task<Tuple<User, List<Habit>>> DeleteHabitAsync(string user_name, string name);
+    Task<Tuple<User, List<Habit>>> DeleteHabitsAsync(string name);
+    Task<User> ChangeSettingsAsync(UserSettings settings);
+    Task DeleteUserAsync(string username);
+
+    //По имени пользователя, телефону и паролю создает нового пользователя
     User CreateUser(string username, PhoneNumber phone_number, string password);
     //По имени пользователя и паролю возвращает всю информацию о пользователе включая привычки, расписание
     User LogIn(string username, string password);
@@ -13,10 +22,9 @@ public interface ITaskTracker
     и словарь с нераспределенными привычками (которые распределились не на все указанное количество дней)
     */
     Tuple<User, List<Habit>> ImportNewShedule(string user_name, string path);
-    //Добавляет привычку для указанного пользователя
     Tuple<User, List<Habit>> AddHabit(Habit habit);
     Tuple<User, List<Habit>> DeleteHabit(string user_name, string name);
     Tuple<User, List<Habit>> DeleteHabits(string name);
-    public User ChangeSettings(UserSettings settings);
+    User ChangeSettings(UserSettings settings);
     void DeleteUser(string username);
 }
--- lab3/code/Domain/TaskTracker.cs
+++ lab3/code/Domain/TaskTracker.cs
@@ -26,7 +26,16 @@ public class TaskTracker : ITaskTracker
         _logger = logger;
         _logger.LogInformation("TaskTracker был успешно инициализирован");
     }
-
+    private async Task<User> GetUserAsync(string user_name)
+    {
+        var user = await _userRepo.TryFullGetAsync(user_name);
+        if (user == null) throw new Exception("Ошибка получения информации о пользователе");
+        var events = await _eventRepo.TryGetAsync(user_name);
+        if (events == null) throw new Exception("Ошибка получения информации о событиях пользователя");
+        user.Events.Clear();
+        user.Events.AddRange(events);
+        return user;
+    }
     /*Функция по username получает всю информацию о пользователе, его привычках, событиях расписания и настройках*/
     private User GetUser(string user_name)
     {
@@ -38,7 +47,19 @@ public class TaskTracker : ITaskTracker
         user.Events.AddRange(events);
         return user;
     }
-
+    public async Task<User> CreateUserAsync(string user_name, PhoneNumber phone_number, string password)
+    {
+        _logger.LogInformation($"Пользователь запросил создание аккаунта с именем пользователя {user_name} и номером телефона {phone_number}");
+        var s = new UserSettings(Guid.NewGuid(), true, user_name, []);
+        var u = new User(user_name, password, phone_number, s);
+        if (!await _userRepo.TryCreateAsync(u))
+        {
+            _logger.LogInformation($"Аккаунт не был создан так как уже существует аккаунт с именем пользователя {user_name}");
+            throw new Exception($"Аккаунт не был создан так как уже существует аккаунт с именем пользователя {user_name}");
+        }
+        _logger.LogInformation($"Аккаунт {user_name} был успешно создан");
+        return await GetUserAsync(u.NameID);
+    }
     /*Функция создания пользователя по имени пользователя, номеру телефона и паролю
      Возвращает полную информацию о пользователе если такого пользователя в системе еще нет
      иначе возвращает null*/
@@ -55,7 +76,23 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Аккаунт {user_name} был успешно создан");
         return GetUser(u.NameID);
     }
-
+    public async Task<User> LogInAsync(string user_name, string password)
+    {
+        _logger.LogInformation($"Пользователь запросил вход в аккаунт с именем {user_name}");
+        var u = await _userRepo.TryGetAsync(user_name);
+        if (u == null)
+        {
+            _logger.LogInformation($"Вход в аккаунт {user_name} не был выполнен так как такого пользователя не существует");
+            throw new Exception($"Вход в аккаунт {user_name} не был выполнен так как такого пользователя не существует");
+        }
+        if (u.PasswordHash != password)
+        {
+            _logger.LogInformation($"Вход в аккаунт {user_name} не был выполнен так как пользователь ввел неправильный пароль");
+            throw new Exception($"Вход в аккаунт {user_name} не был выполнен так как пользователь ввел неправильный пароль");
+        }
+        _logger.LogInformation($"Вход в аккаунт {user_name} был успешно выполнен");
+        return await GetUserAsync(u.NameID);
+    }
     /*Функция входа в аккаунт пользователя по имени пользователя и паролю
      Возвращает полную информацию о пользователе если такой пользователь существует и пароль верен
      иначе возвращает null*/
@@ -76,7 +113,45 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Вход в аккаунт {user_name} был успешно выполнен");
         return GetUser(u.NameID);
     }
+    public async Task<Tuple<User, List<Habit>>> ImportNewSheduleAsync(string user_name, string path)
+    {
+        _logger.LogInformation($"Пользователь с именем {user_name} запросил импорт нового расписания из файла {path}");
+        if (await _userRepo.TryGetAsync(user_name) == null)
+        {
+            _logger.LogError($"Пользователя с именем {user_name} не существует в базе данных");
+            throw new Exception($"Пользователя с именем {user_name} не существует в базе данных");
+        }
+        List<Event> events;
+        try
+        {
+            events = await _shedLoader.LoadSheduleAsync(user_name, path);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning($"Ошибка загрузки расписания для пользователя {user_name}: {ex.Message}");
+            throw new Exception($"Ошибка загрузки расписания для пользователя {user_name}: {ex.Message}");
+        }
+        var habits = await _habitRepo.TryGetAsync(user_name);
+        if (habits == null)
+        {
+            _logger.LogError($"Не удалось получить привычки для пользователя {user_name}");
+            throw new Exception($"Не удалось получить привычки для пользователя {user_name}");
+        }
+        List<Habit> no_distributed = _distributer.DistributeHabits(habits, events);
 
+        if (!await _eventRepo.TryReplaceEventsAsync(events, user_name))
+        {
+            _logger.LogError($"Ошибка при попытке перезаписи событий пользователя {user_name} в базу данных");
+            throw new Exception($"Ошибка при попытке перезаписи событий пользователя {user_name} в базу данных");
+        }
+        if (!await _habitRepo.TryReplaceHabitsAsync(habits, user_name))
+        {
+            _logger.LogError($"Ошибка при попытке перезаписи привычек пользователя {user_name} в базу данных");
+            throw new Exception($"Ошибка при попытке перезаписи привычек пользователя {user_name} в базу данных");
+        }
+        _logger.LogInformation($"Импорт нового расписания для пользователя {user_name} из файла {path} произошел успешно");
+        return new Tuple<User, List<Habit>>(await GetUserAsync(user_name), no_distributed);
+    }
     /*Функция импорта нового расписания для пользователя с именем-идентификатором user_name
      Возвращает кортеж из информации о пользователе и информации о нераспределенных привычках*/
     public Tuple<User, List<Habit>> ImportNewShedule(string user_name, string path)
@@ -118,7 +193,43 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Импорт нового расписания для пользователя {user_name} из файла {path} произошел успешно");
         return new Tuple<User, List<Habit>>(GetUser(user_name), no_distributed);
     }
+    public async Task<Tuple<User, List<Habit>>> AddHabitAsync(Habit habit)
+    {
+        _logger.LogInformation($"Пользователь с именем {habit.UserNameID} запросил добавление привычки {habit.Name}");
+        if (await _userRepo.TryGetAsync(habit.UserNameID) == null)
+        {
+            _logger.LogError($"Пользователя с именем {habit.UserNameID} не существует в базе данных");
+            throw new Exception($"Пользователя с именем {habit.UserNameID} не существует в базе данных");
+        }
 
+        var events = await _eventRepo.TryGetAsync(habit.UserNameID);
+        if (events == null)
+        {
+            _logger.LogError($"Не удалось получить события для пользователя {habit.UserNameID}");
+            throw new Exception($"Не удалось получить события для пользователя {habit.UserNameID}");
+        }
+        var habits = await _habitRepo.TryGetAsync(habit.UserNameID);
+        if (habits == null)
+        {
+            _logger.LogError($"Не удалось получить привычки для пользователя {habit.UserNameID}");
+            throw new Exception($"Не удалось получить привычки для пользователя {habit.UserNameID}");
+        }
+        habits.Add(habit);
+        List<Habit> no_distributed = _distributer.DistributeHabits(habits, events);
+
+        if (!await _eventRepo.TryReplaceEventsAsync(events, habit.UserNameID))
+        {
+            _logger.LogError($"Ошибка при попытке перезаписи событий пользователя {habit.UserNameID} в базу данных");
+            throw new Exception($"Ошибка при попытке перезаписи событий пользователя {habit.UserNameID} в базу данных");
+        }
+        if (!await _habitRepo.TryReplaceHabitsAsync(habits, habit.UserNameID))
+        {
+            _logger.LogError($"Ошибка при попытке перезаписи привычек пользователя {habit.UserNameID} в базу данных");
+            throw new Exception($"Ошибка при попытке перезаписи привычек пользователя {habit.UserNameID} в базу данных");
+        }
+        _logger.LogInformation($"Привычка {habit.Name} была успешно добавлена для пользователя {habit.UserNameID}");
+        return new Tuple<User, List<Habit>>(await GetUserAsync(habit.UserNameID), no_distributed);
+    }
     /*Функция добавления привычки для пользователя с именем-идентификатором user_name
      Возвращает кортеж из информации о пользователе и информации о нераспределенных привычках*/
     public Tuple<User, List<Habit>> AddHabit(Habit habit)
@@ -158,7 +269,44 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Привычка {habit.Name} была успешно добавлена для пользователя {habit.UserNameID}");
         return new Tuple<User, List<Habit>>(GetUser(habit.UserNameID), no_distributed);
     }
+    public async Task<Tuple<User, List<Habit>>> DeleteHabitAsync(string user_name, string name)
+    {
+        _logger.LogInformation($"Пользователь с именем {user_name} запросил удаление привычки {name}");
+        if (await _userRepo.TryGetAsync(user_name) == null)
+        {
+            _logger.LogError($"Пользователя с именем {user_name} не существует в базе данных");
+            throw new Exception($"Пользователя с именем {user_name} не существует в базе данных");
+        }
 
+        var events = await _eventRepo.TryGetAsync(user_name);
+        if (events == null)
+        {
+            _logger.LogError($"Не удалось получить события для пользователя {user_name}");
+            throw new Exception($"Не удалось получить события для пользователя {user_name}");
+        }
+        var habits = await _habitRepo.TryGetAsync(user_name);
+        if (habits == null)
+        {
+            _logger.LogError($"Не удалось получить привычки для пользователя {user_name}");
+            throw new Exception($"Не удалось получить привычки для пользователя {user_name}");
+        }
+
+        habits.RemoveAll(h => h.Name == name);
+        List<Habit> no_distributed = _distributer.DistributeHabits(habits, events);
+
+        if (!await _eventRepo.TryReplaceEventsAsync(events, user_name))
+        {
+            _logger.LogError($"Ошибка при попытке перезаписи событий пользователя {user_name} в базу данных");
+            throw new Exception($"Ошибка при попытке перезаписи событий пользователя {user_name} в базу данных");
+        }
+        if (!await _habitRepo.TryReplaceHabitsAsync(habits, user_name))
+        {
+            _logger.LogError($"Ошибка при попытке перезаписи привычек пользователя {user_name} в базу данных");
+            throw new Exception($"Ошибка при попытке перезаписи привычек пользователя {user_name} в базу данных");
+        }
+        _logger.LogInformation($"Удаление привычки {name} для пользователя {user_name} произведено успешно");
+        return new Tuple<User, List<Habit>>(await GetUserAsync(user_name), no_distributed);
+    }
     /*Функция удаления привычки для пользователя с именем-идентификатором user_name
      Возвращает кортеж из информации о пользователе и информации о нераспределенных привычках*/
     public Tuple<User, List<Habit>> DeleteHabit(string user_name, string name)
@@ -199,6 +347,28 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Удаление привычки {name} для пользователя {user_name} произведено успешно");
         return new Tuple<User, List<Habit>>(GetUser(user_name), no_distributed);
     }
+    public async Task<Tuple<User, List<Habit>>> DeleteHabitsAsync(string user_name)
+    {
+        _logger.LogInformation($"Пользователь с именем {user_name} запросил удаление всех своих привычек");
+        if (await _userRepo.TryGetAsync(user_name) == null)
+        {
+            _logger.LogError($"Пользователя с именем {user_name} не существует в базе данных");
+            throw new Exception($"Пользователя с именем {user_name} не существует в базе данных");
+        }
+        var habits = await _habitRepo.TryGetAsync(user_name);
+        if (habits == null)
+        {
+            _logger.LogError($"Не удалось получить привычки для пользователя {user_name}");
+            throw new Exception($"Не удалось получить привычки для пользователя {user_name}");
+        }
+        if (!await _habitRepo.TryDeleteHabitsAsync(user_name))
+        {
+            _logger.LogError($"Не удалось удалить привычки для пользователя {user_name}");
+            throw new Exception($"Не удалось удалить привычки для пользователя {user_name}");
+        }
+        _logger.LogInformation($"Удаление привычек для пользователя {user_name} произведено успешно");
+        return new Tuple<User, List<Habit>>(await GetUserAsync(user_name), []);
+    }
     public Tuple<User, List<Habit>> DeleteHabits(string user_name)
     {
         _logger.LogInformation($"Пользователь с именем {user_name} запросил удаление всех своих привычек");
@@ -221,7 +391,22 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Удаление привычек для пользователя {user_name} произведено успешно");
         return new Tuple<User, List<Habit>>(GetUser(user_name), []);
     }
-
+    public async Task<User> ChangeSettingsAsync(UserSettings settings)
+    {
+        _logger.LogInformation($"Пользователь с именем {settings.UserNameID} запросил изменение своих настроек");
+        if (await _userRepo.TryGetAsync(settings.UserNameID) == null)
+        {
+            _logger.LogError($"Пользователя с именем {settings.UserNameID} не существует в базе данных");
+            throw new Exception($"Пользователя с именем {settings.UserNameID} не существует в базе данных");
+        }
+        if (!await _userRepo.TryUpdateSettingsAsync(settings))
+        {
+            _logger.LogError($"Не удалось изменить настройки для пользователя {settings.UserNameID}");
+            throw new Exception($"Не удалось изменить настройки для пользователя {settings.UserNameID}");
+        }
+        _logger.LogInformation($"Изменение настроек для пользователя {settings.UserNameID} произведено успешно");
+        return await GetUserAsync(settings.UserNameID);
+    }
     public User ChangeSettings(UserSettings settings)
     {
         _logger.LogInformation($"Пользователь с именем {settings.UserNameID} запросил изменение своих настроек");
@@ -238,7 +423,17 @@ public class TaskTracker : ITaskTracker
         _logger.LogInformation($"Изменение настроек для пользователя {settings.UserNameID} произведено успешно");
         return GetUser(settings.UserNameID);
     }
-
+    public async Task DeleteUserAsync(string user_name)
+    {
+        _logger.LogInformation($"Пользователь с именем {user_name} запросил удаление своей учетной записи");
+        var ret = await _userRepo.TryDeleteAsync(user_name);
+        if (!ret)
+        {
+            _logger.LogError($"Не удалось удалить пользователя с именем {user_name}");
+            throw new Exception($"Не удалось удалить пользователя с именем {user_name}");
+        }
+        _logger.LogInformation($"Удаление учетной записи пользователя {user_name} произведено успешно");
+    }
     public void DeleteUser(string user_name)
     {
         _logger.LogInformation($"Пользователь с именем {user_name} запросил удаление своей учетной записи");
